5634fd972a19bbfcc831a4aae218c83f2823262a
Callum Errington <callum.errington@coxauto.co.uk>
2017-03-03 17:27:53 +0000

    added fake http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+
+import { AppRoutingModule, routedComponents } from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './shared/services/in-memory-data.service';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule, routedComponents } from './app-routing.module';
-import { HeroMiniDetailComponent } from './heroes.component';
+import { HeroMiniDetailComponent, HeroSearchComponent } from './heroes.component';
 import { SidebarComponent } from './shared/sidebar.component/sidebar.component';
 import { HeroService } from 'app/shared/services/hero.service';
 
@@ -15,12 +19,14 @@ import { HeroService } from 'app/shared/services/hero.service';
     AppComponent,
     routedComponents,
     HeroMiniDetailComponent,
+    HeroSearchComponent,
     SidebarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   providers: [HeroService],
--- src/app/heroes.component/index.ts
+++ src/app/heroes.component/index.ts
@@ -1,8 +1,10 @@
 import { HeroDetailComponent } from './hero-detail.component/hero-detail.component';
 import { HeroListComponent } from './hero-list.component/hero-list.component';
 import { HeroMiniDetailComponent } from './hero-mini-detail.component/hero-mini-detail.component';
+import { HeroSearchComponent } from './hero-search.component/hero-search.component';
 
 export * from './heroes.component';
 export { HeroDetailComponent };
 export { HeroListComponent };
 export { HeroMiniDetailComponent };
+export { HeroSearchComponent }
