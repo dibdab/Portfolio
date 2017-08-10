afae4f839b2c447af535b108e08610244c857057
Callum Errington <callum.errington@coxauto.co.uk>
2017-08-10 15:53:05 +0100

    Changed sizea of varioes fixs to fit material grid better Added new mixin to handle material elevation shadows Changed app html to make background colour change for day night theme work

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { MaterialModule } from './material.module';
 import { DocViewerComponent } from './components/doc-viewer.component/doc-viewer.component';
+import { CardComponent } from './components/card.component/card.component';
+
 
 @NgModule({
     imports: [CommonModule, MaterialModule],
-    exports: [CommonModule, MaterialModule, DocViewerComponent],
-    declarations: [DocViewerComponent],
+    exports: [CommonModule, MaterialModule, DocViewerComponent, CardComponent],
+    declarations: [DocViewerComponent, CardComponent],
     providers: [],
 })
 export class SharedModule {
